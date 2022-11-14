9be66954b3614bc5a331a19856ddfd4d007b9a72
Yin Liu <liu.6630@osu.edu>
2022-11-14 02:13:05 -0500

    Separate class, replace __ with _
    use parent class for easy re-use. __ causes mangling, where _ is used to indicate internal use

--- Algorithms.py
+++ Algorithms.py
@@ -1,23 +1,108 @@
+# %% Library
+
 import numpy as np
 import matplotlib.pyplot as plt
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "sans-serif",
+    "font.sans-serif": ["Helvetica"],
+    "ps.usedistiller": "xpdf"})
 
 
-class Bregman_SoR():
-    def __init__(self,  A, batch_size, x_init, k1, k2, tau, beta, max_iter, R, lmbda=1):
-        self.max_iter = max_iter
-        self.batch_size = batch_size
+class SoR:
+    """ parrent class for SOR algorithms, based on the risk-averse of quadratic function, provide function and gradient estimators for this specific problem
+
+    Attributes:
+        A: samples of A_xi
+        batch_size: batch size for inner and outer function value/gradient samples
+
+    """
+
+    def __init__(self,  A, batch_size):
         self.A = A
+        self.batch_size = batch_size
+
+        self.d = A.shape[0]
+        self.n = A.shape[2]  #
+        self.A_avg = self.A.mean(axis=2)
+
+    def _sample_A(self):
+        # sample the A mattrix
+        idx_sample = np.random.randint(0, self.n, self.batch_size)
+        return self.A[:, :, idx_sample]
+
+    def _get_val_g(self, A_sample, x):
+        # get sample value of inner function g
+        g1 = 1/2 * x.T @ np.mean(A_sample, 2) @ x
+        g2 = 1/2 * np.einsum('j, ijk, i', x, A_sample, x)
+        g2 = np.mean(g2**2)
+
+        return np.array([g1, g2])  # g value
+
+    def _get_val_F(self, x):
+        # get deterministic function value at x
+        temp = np.einsum('j,ijk,i', x, self.A, x)
+        temp = np.mean(temp**2)
+
+        return -1/2 * x.T @ self.A_avg @ x + self.lmbda*(temp - 1/4*(x.T @ self.A_avg @ x)**2)
+
+    def _get_grad_h(self, x):
+        # get gradient of geneartining function h(x)
+        return self.k1 * x + self.k2 * np.linalg.norm(x)**2 * x
+
+    def _get_grad_g(self, A_sample, x):
+        # get sample gradient of inner function g
+        grad_g1 = np.einsum('ijk,j->ik', A_sample, x)
+        grad_g21 = np.einsum('j,ijk,i', x, A_sample, x)
+        grad_g2 = grad_g21 * grad_g1
+
+        return np.row_stack([grad_g1.mean(axis=1), grad_g2.mean(axis=1)])
+
+    def _get_grad_f(self, u):
+        # get gradient of outter function f(which is deterministic )
+        return np.array([-1 - 2 * self.lmbda * u[0], self.lmbda])
+
+
+
+####################################
+
+class Bregman_SoR(SoR):
+    """
+    Bregman method for SoR problem
+
+    Attributes:
+        A: samples of A_xi
+        batch_size: batch size for inner and outer function value/gradient samples
+        max_iter: maximum iteration number of the algorithm
+        x_init: initial x
+        k1, k2: coefs of h_g and h_f
+        tau: step size
+        beta: moving average coef
+        R: radius of feasible set
+        d: length of variable x
+        n: number of samples of A_xi
+        A_avg: E[A_xi]
+
+    Result Attributes:
+        x_traj: trajectory of x
+        x_hat_traj: trajectory of x_hat
+        val_F_traj: trajectory of deterministic function value
+        Dh1_x_hat, Dh2_x_hat: D_h(x_hat^{k+1}, x_hat^k)
+        Dh1_x_hat_avg, Dh1_x_hat_avg: 1/k sum_0^k D_h(x_hat^{k+1}, x_hat^k)
+
+
+    """
+
+    def __init__(self,  A, batch_size, x_init, k1, k2, tau, beta, max_iter, R, lmbda=1):
+        super().__init__(A, batch_size)
+        self.max_iter = max_iter
         self.x_init = x_init  # initial x
         self.k1 = k1
         self.k2 = k2
         self.tau = tau
         self.beta = beta
         self.lmbda = lmbda
-        self.R = R  # radius of feasible set
-
-        self.d = A.shape[0]
-        self.n = A.shape[2]  # number of total samples
-        self.A_avg = self.A.mean(axis=2)
+        self.R = R
 
         self.x_traj = np.zeros([self.d, self.max_iter])  # traj of x
         # traj of x_hat(calculated based on determinastic function)
@@ -28,40 +113,13 @@ class Bregman_SoR():
         self.Dh1_x_hat = []
         # D_h2(x_hat^{k+1}- x_hat^k) with tau and k2
         self.Dh2_x_hat = []
+        # average of all previous iterations
+        self.Dh1_x_hat_avg = []
+        self.Dh2_x_hat_avg = []
 
-    def __get_val_g(self, A_sample, x):
-        # get sample value of inner function g
-        g1 = 1/2 * x.T @ np.mean(A_sample, 2) @ x
-        g2 = 1/2 * np.einsum('j, ijk, i', x, A_sample, x)
-        g2 = np.mean(g2**2)
-
-        return np.array([g1, g2])  # g value
-
-    def __get_val_F(self, x):
-        temp = np.einsum('j,ijk,i', x, self.A, x)
-        temp = np.mean(temp**2)
-
-        return -1/2 * x.T @ self.A_avg @ x + self.lmbda*(temp - 1/4*(x.T @ self.A_avg @ x)**2)
-
-    def __get_grad_h(self, x):
-        # get gradient of geneartining function h(x)
-        return self.k1 * x + self.k2 * np.linalg.norm(x)**2 * x
-
-    def __get_grad_g(self, A_sample, x):
-        # get sample gradient of inner function g
-        grad_g1 = np.einsum('ijk,j->ik', A_sample, x)
-        grad_g21 = np.einsum('j,ijk,i', x, A_sample, x)
-        grad_g2 = grad_g21 * grad_g1
-
-        return np.row_stack([grad_g1.mean(axis=1), grad_g2.mean(axis=1)])
-
-    def __get_grad_f(self, u):
-        # get gradient of outter function f(which is deterministic )
-        return np.array([-1 - 2 * self.lmbda * u[0], self.lmbda])
-
-    def __solve_Breg_sub(self, x, w):
+    def _solve_Breg_sub(self, x, w):
         # solve the Bregman subproblem
-        grad_h = self.__get_grad_h(x)
+        grad_h = self._get_grad_h(x)
         t = self.tau * w - grad_h
         if np.all(t == 0):
             y = t
@@ -75,61 +133,54 @@ class Bregman_SoR():
 
         return y
 
-    def __update_u(self, A_sample, u, x_pre, x):
-        g_pre = self.__get_val_g(A_sample, x_pre)
-        g = self.__get_val_g(A_sample, x)
+    def _update_u(self, A_sample, u, x_pre, x):
+        g_pre = self._get_val_g(A_sample, x_pre)
+        g = self._get_val_g(A_sample, x)
 
         u = (1 - self.beta) * (u + g - g_pre) + self.beta * g
 
         return u
 
-    def __sample_A(self):
-        idx_sample = np.random.randint(0, self.n, self.batch_size)
-        return self.A[:, :, idx_sample]
-
     def train(self):
         x = self.x_init
 
         # initial sample
-        A_sample = self.__sample_A()
-        u = self.__get_val_g(A_sample, x)
-        v = self.__get_grad_g(A_sample, x)
-        s = self.__get_grad_f(u)
+        A_sample = self._sample_A()
+        u = self._get_val_g(A_sample, x)
+        v = self._get_grad_g(A_sample, x)
+        s = self._get_grad_f(u)
         w = v.T @ s
 
+        w = np.random.randn(self.d)
+        u = np.random.randn(2)
         for iter in range(self.max_iter):
             x_pre = x
-            x = self.__solve_Breg_sub(x, w)
+            x = self._solve_Breg_sub(x, w)
 
-            A_sample = self.__sample_A()
-            u = self.__update_u(A_sample, u, x_pre, x)
-            v = self.__get_grad_g(A_sample, x)
-            s = self.__get_grad_f(u)
+            A_sample = self._sample_A()
+            u = self._update_u(A_sample, u, x_pre, x)
+            v = self._get_grad_g(A_sample, x)
+            s = self._get_grad_f(u)
             w = v.T @ s
 
             self.x_traj[:, iter] = x
 
             # calculate x_hat
-            val_g = self.__get_val_g(self.A, x)
-            v_det = self.__get_grad_g(self.A, x)
-            s_det = self.__get_grad_f(val_g)
+            val_g = self._get_val_g(self.A, x)
+            v_det = self._get_grad_g(self.A, x)
+            s_det = self._get_grad_f(val_g)
             grad_F = v_det.T @ s_det
-            x_hat = self.__solve_Breg_sub(x, grad_F)
+            x_hat = self._solve_Breg_sub(x, grad_F)
             self.x_hat_traj[:, iter] = x_hat
 
-            self.val_F_traj[iter] = self.__get_val_F(x)
+            self.val_F_traj[iter] = self._get_val_F(x)
 
-    def plot(self):
-        plt.rcParams.update({
-            "text.usetex": True,
-            "font.family": "sans-serif",
-            "font.sans-serif": ["Helvetica"],
-            "ps.usedistiller": "xpdf"})
-
+    def calculate_Dh(self):
+        # calculate Dh_1 and Dh_2 for x_hat
         self.Dh1_x_hat = self.x_hat_traj[:, 1:] - self.x_traj[:, 0:-1]
         # self.Dh1_x_hat = self.k1 / \
         #     (self.tau**2) * np.sum(self.Dh1_x_hat * self.Dh1_x_hat, axis=0)
-        self.Dh1_x_hat =  np.sum(self.Dh1_x_hat * self.Dh1_x_hat, axis=0)
+        self.Dh1_x_hat = np.sum(self.Dh1_x_hat * self.Dh1_x_hat, axis=0)
 
         self.Dh2_x_hat = np.zeros(self.max_iter - 1)
 
@@ -141,13 +192,43 @@ class Bregman_SoR():
             self.Dh2_x_hat[iter] = (np.linalg.norm(
                 y)**4 / 4 - np.linalg.norm(x)**4 / 4 - np.linalg.norm(x)**2 * x @ (y-x))
 
-        fig, (ax1, ax2) = plt.subplots(1, 2, figsize =(6,4))
-        fig.suptitle(f"k1={self.k1: .2f}, k2 = {self.k2: .2f}")
-        ax1.plot(self.Dh1_x_hat)
-        ax1.set_ylabel(r"$D_{h_1}(\hat{x}^{k+1}-x^k)/\tau^2$")
+    def calculate_Dh_avg(self):
+        self.calculate_Dh()
 
-        ax2.plot(self.Dh2_x_hat)
-        ax2.set_ylabel(r"$D_{h_2}(\hat{x}^{k+1}-x^k)/\tau^2$")
+        self.Dh1_x_hat_avg = np.zeros_like(self.Dh1_x_hat)
+        self.Dh2_x_hat_avg = np.zeros_like(self.Dh2_x_hat)
+
+        for i in range(self.max_iter - 1):
+            self.Dh1_x_hat_avg[i] = np.mean(self.Dh1_x_hat[:i + 1])
+            self.Dh2_x_hat_avg[i] = np.mean(self.Dh2_x_hat[:i + 1])
+
+    def plot(self, avg=True):
+        # if avg = True, plot the averaged D_h, if = False, plot each iteration
+
+        fig, (ax1, ax2, ax3, ax4) = plt.subplots(1, 4, figsize=(12, 4))
+        fig.suptitle(f"k1={self.k1: .2f}, k2 = {self.k2: .2f}")
+
+        if avg == True:
+            if self.Dh1_x_hat_avg == []:
+                self.calculate_Dh_avg()
+            ax1.plot(self.k1 * self.Dh1_x_hat_avg / self.tau**2)
+            ax1.set_ylabel(r"$E[D_{h_1}(\hat{x}^{k+1}-x^k)/\tau^2]$")
+
+            ax2.plot(self.k2 * self.Dh2_x_hat_avg / self.tau**2)
+            ax2.set_ylabel(r"$E[D_{h_2}(\hat{x}^{k+1}-x^k)/\tau^2]$")
+            ax4.plot((self.k1 * self.Dh1_x_hat_avg / self.tau**2 +
+                     self.k2 * self.Dh2_x_hat_avg / self.tau**2))
+
+        else:
+            if self.Dh1_x_hat == []:
+                self.calculate_Dh()
+            ax1.plot(self.Dh1_x_hat)
+            ax1.set_ylabel(r"$D_{h_1}(\hat{x}^{k+1}-x^k)/\tau^2$")
+
+            ax2.plot(self.Dh2_x_hat)
+            ax2.set_ylabel(r"$D_{h_2}(\hat{x}^{k+1}-x^k)/\tau^2$")
+        ax3.plot(self.val_F_traj)
+        ax3.set_ylabel(r"$F(x^k)$")
 
         for ax in fig.get_axes():
             ax.set_xlabel("iteration")
@@ -158,5 +239,4 @@ class Bregman_SoR():
         # fig.show()
 
 
-if __name__ == '__main__':
-    pass
+
